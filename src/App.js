f03246c0b7cd2948714cac5de2b4755be8b4b545
pryority <matthewapryor@gmail.com>
2022-07-11 11:32:13 -0400

    initial - add react, tailwind

--- src/App.js
+++ src/App.js
@@ -3,21 +3,9 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex flex-col items-center w-full">
+      <h1 className='text-3xl'>Password Generator</h1>
+      <button className='bg-blue-400 font-medium px-6 py-2 rounded-lg border border-2 border-slate-300  hover:text-white'>Generate</button>
     </div>
   );
 }
